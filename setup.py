b452ceb14e8c03f065c3bb58c7c4c37b5870399a
Anton Gilgur <agilgur5@gmail.com>
2019-08-07 20:08:01 -0400

    (pkg): add Django 2.2 classifier
    - seems like none of the backwards-incompatible changes affect this
      library, so if it worked with 2.1, should work with 2.2
      - c.f. https://docs.djangoproject.com/en/2.2/releases/2.2/#backwards-incompatible-2-2

--- setup.py
+++ setup.py
@@ -43,6 +43,7 @@ setup(
         'Framework :: Django :: 1.11',
         'Framework :: Django :: 2.0',
         'Framework :: Django :: 2.1',
+        'Framework :: Django :: 2.2',
     ],
     keywords=('django api rest ad-hoc decorator json dict form formset ' +
               'validation'),
