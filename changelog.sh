agilgur5/django-serializable-model

(pkg): add script to generate changelog for each release
- makes my job easier and makes it possible for others to replicate
  the same style changelog easily

- use my new fork of changelog-maker to make it a one-liner :)
- use sed to workaround lack of package.json / lack of ability to read
  setup.py

changelog.sh
@@ -1,0 +1,2 @@
+# changelog-maker only gets name from package.json, so do a replace
+npx @agilgur5/changelog-maker | sed 's_nodejs/node_agilgur5/django-serializable-model_';

agilgur5/django-api-decorators

changelog.sh
@@ -1,0 +1,2 @@
+# changelog-maker only gets name from package.json, so do a replace
+npx @agilgur5/changelog-maker | sed 's_nodejs/node_agilgur5/django-api-decorators_';